861a797a902548afd2135d3cfb3644f4fa3dff1e
Seaborn Lee <seabornlee.cn@gmail.com>
2016-08-03 17:21:04 +0800

    Toggle editor and previewer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 const App = React.createClass({
+    getInitialState: function() {
+        return {
+            isEditor: true
+        }
+    },
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        });
+    },
     render: function() {
-        return <div>Hello!</div>;
+        return <div>
+            <button onClick={this.toggle}>{this.state.isEditor ? "Preview" : "Edit"}</button>
+            <div className={this.state.isEditor ? "" : "hidden"}>
+                <Editor />
+            </div>
+            <div className={this.state.isEditor ? "hidden" : ""}>
+                <Previewer />
+            </div>
+        </div>;
     }
 });
+
+const Editor = React.createClass({
+    render: function () {
+        return <div>Editor</div>;
+    }
+});
+
+const Previewer = React.createClass({
+    render: function () {
+        return <div>Previewer</div>;
+    }
+});
+
+
 
 ReactDOM.render(<App />, document.getElementById('content'));
