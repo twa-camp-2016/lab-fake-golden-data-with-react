44868ed8fb59536afda22abeef6e701b0dd7ffba
Seaborn Lee <seabornlee.cn@gmail.com>
2016-08-03 14:23:08 +0800

    Toggle preview and editor

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 const App = React.createClass({
-    render: () => {
-        return <div>Hello React!</div>;
+    getInitialState: function () {
+        return {
+            isPreview: true
+        };
+    },
+    toggle: function() {
+        this.setState({
+            isPreview: !this.state.isPreview
+        });
+    },
+    render: function () {
+        const text = this.state.isPreview ? '编辑' : '预览';
+        return <div>
+            <button onClick={this.toggle}>{text}</button>
+            <Editor className={this.state.isPreview ? "hidden" : ""} />
+            <Preview className={this.state.isPreview ? "" : "hidden"} />
+        </div>;
+    }
+});
+
+const Editor = React.createClass({
+    render: function () {
+        return <div className={this.props.className}>Editor</div>;
+    }
+});
+
+const Preview = React.createClass({
+    render: function () {
+        return <div className={this.props.className}>Preview</div>;
     }
 });
 
